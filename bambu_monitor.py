50efe2b47862acaa2c6c2f275ba969ca21959dcc
Comxx <16421418+Comxx@users.noreply.github.com>
2024-04-18 12:29:42 -0700

    fixs

--- bambu_monitor.py
+++ bambu_monitor.py
@@ -17,7 +17,6 @@ DASH = '\n-------------------------------------------\n'
 PO_TITLE = "Testing Bambu Printer"
 PO_SOUND = 'classical'
 # Add a global variable to store the last known gcode_state
-gcode_state = ''
 last_gcode_state = ''
 # Global state
 first_run = False
@@ -85,7 +84,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
     client.subscribe("device/"+device_id+"/report", 0)
 
 def on_message(client, userdata, msg):
-    global last_gcode_state, first_run, message_sent, gcode_state
+    global last_gcode_state, first_run, message_sent
     english_errors = []
     message_sent = False
     
@@ -94,25 +93,21 @@ def on_message(client, userdata, msg):
         dataDict = json.loads(msgData)
 
         english_errors = fetch_english_errors()
+        
         if 'print' in dataDict and 'gcode_state' in dataDict['print']:
             gcode_state = dataDict['print']['gcode_state']
-        
-        if gcode_state != last_gcode_state:
-            process_print_data(dataDict, client, english_errors)
-            last_gcode_state = gcode_state
-        
-    except json.JSONDecodeError as json_error:
-        logging.error("Failed to decode JSON from MQTT message: {}".format(json_error))
-    except requests.exceptions.RequestException as request_error:
-        logging.error("Failed to fetch data: {}".format(request_error))
-    except ValueError as value_error:
-        logging.error("Value error: {}".format(value_error))
+            
+            if gcode_state != last_gcode_state:
+                process_print_data(dataDict, client, english_errors)
+                last_gcode_state = gcode_state
+
+    except json.JSONDecodeError as e:
+        logging.error("Failed to decode JSON from MQTT message: {e}")
     except Exception as e:
         logging.error(f"Unexpected error in on_message: {e}")
 
-
 def process_print_data(dataDict, client, english_errors):
-    global first_run, message_sent, gcode_state
+    global first_run, message_sent
     msg_text = "<ul>"
     priority = 0
     hms_data = dataDict['print'].get('hms', [{'attr': 0, 'code': 0}])
@@ -132,80 +127,71 @@ def process_print_data(dataDict, client, english_errors):
     error_code_to_hms_cleaned = str(device__HMS_error_code).replace('_', '')
     found_device_error = search_error(error_code_to_hms_cleaned, english_errors)
     
-    ignore_list = ['0c00_0300_0002_0004', '0c00_0300_0003_000b', '0c00_0100_0001_0004'] # list to ignore specific errors
+    if 'print' in dataDict:
+        if 'gcode_state' in dataDict['print']:
+            gcode_state = dataDict['print']['gcode_state']
+            json_formatted_str = json.dumps(dataDict, indent=2)
+            logging.info(DASH + json_formatted_str + DASH)
+            msg_text += "<li>State: " + gcode_state + " </li>"
+        
+        if 'mc_percent' in dataDict['print']:
+            percent_done = dataDict['print']['mc_percent']
+            msg_text += f"<li>Percent: {percent_done}% </li>"
+        
+        if 'subtask_name' in dataDict['print']:
+            msg_text += "<li>Name: " + dataDict['print']['subtask_name'] + " </li>"
     
-    if found_device_error and found_device_error['ecode'] not in ignore_list:
-        if 'print' in dataDict:
-            if 'gcode_state' in dataDict['print']:
-                gcode_state = dataDict['print']['gcode_state']
-                json_formatted_str = json.dumps(dataDict, indent=2)
-                logging.info(DASH + json_formatted_str + DASH)
-                msg_text += "<li>State: " + gcode_state + " </li>"
-            
-            if 'mc_percent' in dataDict['print']:
-                percent_done = dataDict['print']['mc_percent']
-                msg_text += f"<li>Percent: {percent_done}% </li>"
-            
-            if 'subtask_name' in dataDict['print']:
-                msg_text += "<li>Name: " + dataDict['print']['subtask_name'] + " </li>"
+    if 'gcode_start_time' in dataDict['print']:
+        unix_timestamp = float(dataDict['print']['gcode_start_time'])
+        if gcode_state == "PREPARE" and unix_timestamp == 0:
+            unix_timestamp = time.time()
+        if unix_timestamp != 0:
+            local_timezone = tzlocal.get_localzone()
+            local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+            my_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
+            msg_text += "<li>Started: " + my_datetime + " </li>"
+    
+    my_finish_datetime = ""
+    remaining_time = ""
+    
+    if 'mc_remaining_time' in dataDict['print']:
+        time_left_seconds = int(dataDict['print']['mc_remaining_time']) * 60
+        logging.debug("Time left (seconds): {}".format(time_left_seconds))
+        if time_left_seconds != 0:
+            aprox_finish_time = time.time() + time_left_seconds
+            logging.debug("Approx Finish Time (epoch): {}".format(aprox_finish_time))
+            unix_timestamp = float(aprox_finish_time)
+            local_timezone = tzlocal.get_localzone()
+            local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+            logging.debug("Local Time: {}".format(local_time))
+            my_finish_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
+            remaining_time = str(timedelta(seconds=time_left_seconds))
+        else:
+            if gcode_state == "FINISH" and time_left_seconds == 0:
+                my_finish_datetime = "Done!"
+        msg_text += "<li>Aprox End: " + my_finish_datetime + " </li>"
+        msg_text += f"<li>Remaining time: {remaining_time} </li>"
+    
+    if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1) or ('print_error' in dataDict['print'] and dataDict['print']['print_error'] != 0) or gcode_state == "FAILED":
+        msg_text += f"<li>print_error: {dataDict['print'].get('print_error', 'N/A')}</li>"
+        msg_text += f"<li>mc_print_error_code: {dataDict['print'].get('mc_print_error_code', 'N/A')}</li>"
+        msg_text += f"<li>HMS code: {device__HMS_error_code}</li>"
+        msg_text += f"<li>Description: {found_device_error['intro']}</li>"
+        if device__HMS_error_code:
+            msg_text += f"<li>URL: https://wiki.bambulab.com/en/x1/troubleshooting/hmscode/{error_code_to_hms_cleaned}</li>"
+        error_code = int(dataDict['print'].get('mc_print_error_code', 0))
+        fail_reason = "Print Canceled" if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1 and dataDict['print']['fail_reason'] != '50348044') else dataDict['print'].get('fail_reason', 'N/A')
         
-        if 'gcode_start_time' in dataDict['print']:
-            unix_timestamp = float(dataDict['print']['gcode_start_time'])
-            if gcode_state == "PREPARE" and unix_timestamp == 0:
-                unix_timestamp = time.time()
-            if unix_timestamp != 0:
-                local_timezone = tzlocal.get_localzone()
-                local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
-                my_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
-                msg_text += "<li>Started: " + my_datetime + " </li>"
-        
-        my_finish_datetime = ""
-        remaining_time = ""
-        
-        if 'mc_remaining_time' in dataDict['print']:
-            time_left_seconds = int(dataDict['print']['mc_remaining_time']) * 60
-           # logging.debug("Time left (seconds): {}".format(time_left_seconds))
-            if time_left_seconds != 0:
-                aprox_finish_time = time.time() + time_left_seconds
-                #logging.debug("Approx Finish Time (epoch): {}".format(aprox_finish_time))
-                unix_timestamp = float(aprox_finish_time)
-                local_timezone = tzlocal.get_localzone()
-                local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
-               # logging.debug("Local Time: {}".format(local_time))
-                my_finish_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
-                remaining_time = str(timedelta(seconds=time_left_seconds))
-            else:
-                if gcode_state == "FINISH" and time_left_seconds == 0:
-                    my_finish_datetime = "Done!"
-            msg_text += "<li>Aprox End: " + my_finish_datetime + " </li>"
-            msg_text += f"<li>Remaining time: {remaining_time} </li>"
-        
-        if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1) or ('print_error' in dataDict['print'] and dataDict['print']['print_error'] != 0) or gcode_state == "FAILED":
-            msg_text += f"<li>print_error: {dataDict['print'].get('print_error', 'N/A')}</li>"
-            msg_text += f"<li>mc_print_error_code: {dataDict['print'].get('mc_print_error_code', 'N/A')}</li>"
-            msg_text += f"<li>HMS code: {device__HMS_error_code}</li>"
-            msg_text += f"<li>Description: {found_device_error['intro']}</li>"
-            error_code = int(dataDict['print'].get('mc_print_error_code', 0))
-            fail_reason = "Print Canceled" if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1 and dataDict['print']['fail_reason'] != '50348044') else dataDict['print'].get('fail_reason', 'N/A')
-            
-            custom_fail_reasons = {
-                32778: "Arrr! Swab the poop deck!",
-                32771: "Spaghetti and meatballs!",
-                32773: "Didn't pull out!",
-                32774: "Build plate mismatch!",
-                32769: "Let's take a moment to PAUSE!",
-            }
-            # Debugging for custom fail reasons assignment
-            logging.debug("Error code: {}".format(error_code))
-            logging.debug("Previous fail reason: {}".format(fail_reason))
-
-            fail_reason = custom_fail_reasons.get(error_code, fail_reason)
-
-            logging.debug("Updated fail reason: {}".format(fail_reason))
-            msg_text += f"<li>fail_reason: {fail_reason}</li>"
-            priority = 1 # Set higher priority for errors
-        # debugging when the condition is triggered
-        logging.debug("Printer has trigger a error code")
+        custom_fail_reasons = {
+            32778: "Arrr! Swab the poop deck!",
+            32771: "Spaghetti and meatballs!",
+            32773: "Didn't pull out!",
+            32774: "Build plate mismatch!",
+            32769: "Let's take a moment to PAUSE!",
+        }
+        fail_reason = custom_fail_reasons.get(error_code, fail_reason)
+        msg_text += f"<li>fail_reason: {fail_reason}</li>"
+        priority = 1  # Set higher priority for errors
 
         # Check for specific error codes or fail reasons to turn off the lights
         if '50348044' in [dataDict['print'].get('print_error', ''), dataDict['print'].get('fail_reason', '')]:
@@ -284,7 +270,7 @@ def process_print_data(dataDict, client, english_errors):
                     else:
                             first_run = False            
                     message_sent = True
-def main(argv):
+0def main(argv):
     try:
         setup_logging()
         logging.info("Starting")
@@ -301,4 +287,4 @@ def main(argv):
         print("Fatal error Please read Logs")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:])                    
