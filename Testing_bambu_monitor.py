e4690aa80616068dc09f37f1f0fd56b225e2638d
Comxx <16421418+Comxx@users.noreply.github.com>
2024-04-19 20:32:13 -0700

    add error check

--- Testing_bambu_monitor.py
+++ Testing_bambu_monitor.py
@@ -41,100 +41,104 @@ def on_connect(client, userdata, flags, reason_code, properties):
      client.subscribe("device/"+device_id+"/report", 0)
 def on_message(client, userdata, msg):
     global DASH, gcode_state_prev, app, user, my_pushover_app, my_pushover_user, first_run, percent_notify, percent_donetry
-    msgData = msg.payload.decode('utf-8')
-    dataDict = json.loads(msgData)
-    if 'print' in dataDict:
-        hms_data = dataDict['print'].get('hms', [{'attr': 0, 'code': 0}])
-    
-        if hms_data:
-            hms_data = hms_data[0]
-        else:
-            hms_data = {'attr': 0, 'code': 0}
+    try:
+        msgData = msg.payload.decode('utf-8')
+        dataDict = json.loads(msgData)
+        if 'print' in dataDict:
+            hms_data = dataDict['print'].get('hms', [{'attr': 0, 'code': 0}])
         
-        attr = hms_data.get('attr', 0)
-        code = hms_data.get('code', 0)
-        
-        device__HMS_error_code = hms_code(attr, code)
-        
-        english_errors = fetch_english_errors() or []
-        
-        error_code_to_hms_cleaned = str(device__HMS_error_code).replace('_', '')
-        found_device_error = search_error(error_code_to_hms_cleaned, english_errors)
-        
-        gcode_state = dataDict['print']['gcode_state']
-        percent_done = dataDict['print']['mc_percent']
-
-        if gcode_state_prev != gcode_state:
-           
-            priority = 0
-            logging.info("gcode_state has changed to " + gcode_state)
-            json_formatted_str = json.dumps(dataDict, indent=2)
-            logging.info(DASH + json_formatted_str + DASH)
-            gcode_state_prev = gcode_state
-
-            my_datetime = ""
-            if 'gcode_start_time' in dataDict['print']:
-                unix_timestamp = float(dataDict['print']['gcode_start_time'])
-                if gcode_state == "PREPARE" and unix_timestamp == 0:
-                    unix_timestamp = float(time.time())
-                if unix_timestamp != 0:
-                    local_timezone = tzlocal.get_localzone()
-                    local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
-                    my_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
-                else:
-                    my_datetime = ""
-
-            my_finish_datetime = ""
-            remaining_time = ""
-            if 'mc_remaining_time' in dataDict['print']:
-                time_left_seconds = int(dataDict['print']['mc_remaining_time']) * 60
-                if time_left_seconds != 0:
-                    aprox_finish_time = time.time() + time_left_seconds
-                    unix_timestamp = float(aprox_finish_time)
-                    local_timezone = tzlocal.get_localzone()
-                    local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
-                    my_finish_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
-                    remaining_time = str(timedelta(minutes=dataDict['print']['mc_remaining_time']))
-                else:
-                    if gcode_state == "FINISH" and time_left_seconds == 0:
-                        my_finish_datetime = "Done!"
-
-            msg_text = "<ul>"
-            msg_text += "<li>State: " + gcode_state + " </li>"
-            msg_text += f"<li>Percent: {percent_done}% </li>"
-            if 'subtask_name' in dataDict['print']:
-                msg_text += "<li>Name: " + dataDict['print']['subtask_name'] + " </li>"
-            msg_text += f"<li>Remaining time: {remaining_time} </li>"
-            msg_text += "<li>Started: " + my_datetime + "</li>"
-            msg_text += "<li>Aprox End: " + my_finish_datetime + "</li>"
-
-            fail_reason = ""
-            if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1) or ('print_error' in dataDict['print'] and dataDict['print']['print_error'] != 0) or gcode_state == "FAILED":
-                msg_text += f"<li>print_error: {dataDict['print']['print_error']}</li>"
-                msg_text += f"<li>mc_print_error_code: {dataDict['print']['mc_print_error_code']}</li>"
-                msg_text += f"<li>HMS code: {device__HMS_error_code}</li>"
-                msg_text += f"<li>Description: {found_device_error['intro']}</li>"
-                fail_reason = "Print Canceled" if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1 and dataDict['print']['fail_reason'] != '50348044') else dataDict['print']['fail_reason']
-                msg_text += "<li>fail_reason: " + fail_reason + "</li>"
-                priority = 1
-
-            if not first_run:
-                msg_text += "</ul>"
-                message = po_user.create_message(
-                    title=PO_TITLE,
-                    message=msg_text,
-                    url= f"https://wiki.bambulab.com/en/x1/troubleshooting/hmscode/{device__HMS_error_code}" if device__HMS_error_code else "",
-                    html=True,
-                    sound=PO_SOUND,
-                    priority=priority
-                )
-                message.send()
-                #if priority == 1:
-                #    for x in range(repeat_errors):
-                #        time.sleep(pause_error_secs)
-                    #    message.send()
+            if hms_data:
+                hms_data = hms_data[0]
             else:
-                first_run = False
+                hms_data = {'attr': 0, 'code': 0}
+            
+            attr = hms_data.get('attr', 0)
+            code = hms_data.get('code', 0)
+            
+            device__HMS_error_code = hms_code(attr, code)
+            
+            english_errors = fetch_english_errors() or []
+            
+            error_code_to_hms_cleaned = str(device__HMS_error_code).replace('_', '')
+            found_device_error = search_error(error_code_to_hms_cleaned, english_errors)
+            
+            gcode_state = dataDict['print']['gcode_state']
+            percent_done = dataDict['print']['mc_percent']
+
+            if gcode_state_prev != gcode_state:
+            
+                priority = 0
+                logging.info("gcode_state has changed to " + gcode_state)
+                json_formatted_str = json.dumps(dataDict, indent=2)
+                logging.info(DASH + json_formatted_str + DASH)
+                gcode_state_prev = gcode_state
+
+                my_datetime = ""
+                if 'gcode_start_time' in dataDict['print']:
+                    unix_timestamp = float(dataDict['print']['gcode_start_time'])
+                    if gcode_state == "PREPARE" and unix_timestamp == 0:
+                        unix_timestamp = float(time.time())
+                    if unix_timestamp != 0:
+                        local_timezone = tzlocal.get_localzone()
+                        local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+                        my_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
+                    else:
+                        my_datetime = ""
+
+                my_finish_datetime = ""
+                remaining_time = ""
+                if 'mc_remaining_time' in dataDict['print']:
+                    time_left_seconds = int(dataDict['print']['mc_remaining_time']) * 60
+                    if time_left_seconds != 0:
+                        aprox_finish_time = time.time() + time_left_seconds
+                        unix_timestamp = float(aprox_finish_time)
+                        local_timezone = tzlocal.get_localzone()
+                        local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+                        my_finish_datetime = local_time.strftime("%Y-%m-%d %I:%M %p (%Z)")
+                        remaining_time = str(timedelta(minutes=dataDict['print']['mc_remaining_time']))
+                    else:
+                        if gcode_state == "FINISH" and time_left_seconds == 0:
+                            my_finish_datetime = "Done!"
+
+                msg_text = "<ul>"
+                msg_text += "<li>State: " + gcode_state + " </li>"
+                msg_text += f"<li>Percent: {percent_done}% </li>"
+                if 'subtask_name' in dataDict['print']:
+                    msg_text += "<li>Name: " + dataDict['print']['subtask_name'] + " </li>"
+                msg_text += f"<li>Remaining time: {remaining_time} </li>"
+                msg_text += "<li>Started: " + my_datetime + "</li>"
+                msg_text += "<li>Aprox End: " + my_finish_datetime + "</li>"
+
+                fail_reason = ""
+                if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1) or ('print_error' in dataDict['print'] and dataDict['print']['print_error'] != 0) or gcode_state == "FAILED":
+                    msg_text += f"<li>print_error: {dataDict['print']['print_error']}</li>"
+                    msg_text += f"<li>mc_print_error_code: {dataDict['print']['mc_print_error_code']}</li>"
+                    msg_text += f"<li>HMS code: {device__HMS_error_code}</li>"
+                    msg_text += f"<li>Description: {found_device_error['intro']}</li>"
+                    fail_reason = "Print Canceled" if ('fail_reason' in dataDict['print'] and len(dataDict['print']['fail_reason']) > 1 and dataDict['print']['fail_reason'] != '50348044') else dataDict['print']['fail_reason']
+                    msg_text += "<li>fail_reason: " + fail_reason + "</li>"
+                    priority = 1
+
+                if not first_run:
+                    msg_text += "</ul>"
+                    message = po_user.create_message(
+                        title=PO_TITLE,
+                        message=msg_text,
+                        url= f"https://wiki.bambulab.com/en/x1/troubleshooting/hmscode/{device__HMS_error_code}" if device__HMS_error_code else "",
+                        html=True,
+                        sound=PO_SOUND,
+                        priority=priority
+                    )
+                    message.send()
+                    #if priority == 1:
+                    #    for x in range(repeat_errors):
+                    #        time.sleep(pause_error_secs)
+                        #    message.send()
+                else:
+                    first_run = False
+    except Exception as e:
+        logging.error(f"Fatal error in on_message {e}")
+        
 def hms_code(attr, code):
     if not isinstance(attr, int) or attr < 0 or not isinstance(code, int) or code < 0:
         raise ValueError("attr and code must be positive integers")
