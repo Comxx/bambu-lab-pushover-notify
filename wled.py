3716137cc84d3ba41454c27c56c2b859e38d358f
Comxx <16421418+Comxx@users.noreply.github.com>
2024-08-21 13:16:08 -0700

    more logging clean up

--- wled.py
+++ wled.py
@@ -9,32 +9,32 @@ def set_power(ip_address, state):
     payload = {"on": state}
     response = requests.post(url, data=json.dumps(payload))
     if response.status_code == 200:
-        logging.info("Power state set successfully.")
+        logging.debug("Power state set successfully.")
     else:
-        logging.info("Failed to set power state.")
+        logging.error("Failed to set power state.")
 
 def set_brightness(ip_address, brightness):
     url = f"http://{ip_address}/json"
     payload = {"bri": brightness}
     response = requests.post(url, data=json.dumps(payload))
     if response.status_code == 200:
-        logging.info("Brightness set successfully.")
+        logging.debug("Brightness set successfully.")
     else:
-        logging.info("Failed to set brightness.")       
+        logging.error("Failed to set brightness.")       
 def set_color(ip_address, color):
     url = f"http://{ip_address}/json"
     payload = {"seg": [{"col": [[color[0], color[1], color[2]]]}]}
     response = requests.post(url, data=json.dumps(payload))
     if response.status_code == 200:
-        logging.info("Color set successfully.")
+        logging.drbug("Color set successfully.")
     else:
-        logging.info("Failed to set color.")        
+        logging.error("Failed to set color.")        
 
 def set_effect(ip_address, effect):
     url = f"http://{ip_address}/json"
     payload = {"seg": [{"fx": effect}]}
     response = requests.post(url, data=json.dumps(payload))
     if response.status_code == 200:
-        logging.info("Effect set successfully.")
+        logging.debug("Effect set successfully.")
     else:
-        logging.info("Failed to set effect.")   
+        logging.debug("Failed to set effect.")   
